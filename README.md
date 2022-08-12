8fe0c2501ca4d60dbd30ade90b1e8c39ac659c82
EyjiCuff <eyji.koike@gmail.com>
2022-08-12 16:51:18 -0300

    Manage Secret

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# django
+# django  
+This repo is meant to show off some django projects I've worked on.
+
+
+## URL Shortener  
+This url shortener is an application that can be deployed to heroku. It takes a url, generates a uuid that is linked to 
+the original url, and then redirects the user everytime the new url is used.
--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import django_on_heroku
 import dj_database_url
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0jg=!cd+bhbc#ryt=pa9!tlo0%0o8=zc8frgo4f@yde20co*gn"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
